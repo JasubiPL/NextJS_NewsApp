9166e90ca764ca996451cf40fc9d93216359b329
JasubiPL <gameportal7@hotmail.com>
2023-03-21 09:23:01 -0600

    Se limpia aplicacion

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '@/styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <title>NewsApp</title>
+      <meta name='description' content='newsapp - the best app to read news'/>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <header>newsapi</header>
+    <Component {...pageProps} />
+  </>
 }
