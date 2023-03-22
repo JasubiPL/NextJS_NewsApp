4344f35917ee50a640fb735dede90906e6c21398
JasubiPL <gameportal7@hotmail.com>
2023-03-22 09:57:03 -0600

    ServerSideProps y StaticProps

--- components/PageLayout.jsx
+++ components/PageLayout.jsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function PageLayout({children, title = `NewsApp`}){
   return(
@@ -8,12 +9,21 @@ export default function PageLayout({children, title = `NewsApp`}){
       <meta name='description' content='newsapp - the best app to read news'/>
       <link rel="icon" href="/favicon.ico" />
     </Head>
-    <header>newsapi</header>
+    <header>
+      <div>
+        <Link href='/'>News API 🗞</Link>
+      </div>
+      <div>
+        <Link href='/about'>About</Link>
+      </div>
+    </header>
     <main>
       {children}
     </main>
     <style jsx>{`
       header{
+        display:flex;
+        justify-content: space-between;
         padding:20px;
       }
     `}</style>
--- pages/index.js
+++ pages/index.js
@@ -22,12 +22,34 @@ export default function Home({articles}) {
 }
 
 //Hacemos el Fetchin de datos del lado del servidor para que se renderice mas rapido y para optimizacion del CEO
-export async function getServerSideProps() {
-  const response = await fetch('https://newsapi.org/v2/everything?q=tesla&from=2023-02-22&sortBy=publishedAt&apiKey=eb900029ae884c249c4ebc04ce465e17')
-  const { articles } = await response.json()
-  return{
-    props:{
-      articles
-    }
-  }
-}
+
+/*
+getStaticProps()
+  -N Request -> se ejecuta 1 vez en build time(o para refrescar la pagina)
+*/
+//  export async function getStaticProps() {
+//    const response = await fetch('https://newsapi.org/v2/everything?q=tesla&from=2023-02-22&sortBy=publishedAt&apiKey=eb900029ae884c249c4ebc04ce465e17')
+//    const { articles } = await response.json()
+//    return{
+//      props:{
+//        articles
+//      }
+//    }
+//  }
+
+/*
+getServerSideProps()
+  - N request -> se ejcuta N Veces
+  - Para datos que necesitas que sean muy libe
+  - Si tiene demaciados datos dinamicos
+*/
+
+ export async function getServerSideProps() {
+   const response = await fetch('https://newsapi.org/v2/everything?q=tesla&from=2023-02-22&sortBy=publishedAt&apiKey=eb900029ae884c249c4ebc04ce465e17')
+   const { articles } = await response.json()
+   return{
+     props:{
+       articles
+     }
+   }
+ }
