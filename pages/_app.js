6ad936686b30b7c8c738cd2e1597d693395e84d4
JasubiPL <gameportal7@hotmail.com>
2023-03-21 18:00:15 -0600

    Rutas y Fetch

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,5 @@
 import '@/styles/globals.css'
-import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <>
-    <Head>
-      <title>NewsApp</title>
-      <meta name='description' content='newsapp - the best app to read news'/>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <header>newsapi</header>
-    <Component {...pageProps} />
-  </>
+  return <Component {...pageProps} />
 }
---
+++ pages/about.jsx
@@ -0,0 +1,7 @@
+import PageLayout from "@/components/PageLayout";
+
+export default function About(){
+  return <PageLayout>
+      <h1>Esta es la pagina About</h1>
+    </PageLayout>
+}
